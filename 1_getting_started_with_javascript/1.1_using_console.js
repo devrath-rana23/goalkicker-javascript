7285155ceca3c2ef17f526468cf6932f4d709043
Devrath Singh Rana <devrathsinghrana@Devraths-Mac-mini.local>
2022-11-30 00:46:39 +0530

    1

---
+++ 1_getting_started_with_javascript/1.1_using_console.js
@@ -0,0 +1,14 @@
+////// Show this tutorial code in console itself and then write the explaination in paint by taking a screen shot and pasting it in paint
+
+
+////// console.log() is a logging method
+//It is used for debugging purpose
+
+//In the example below, the console.log() function prints Hello, World! to the console and returns undefined (shown below in the console output window). This is because console.log() has no explicit return value.
+
+console.log("Hello World!");
+
+////// Logging variables ////
+
+var foo= "kar";
+console.log(foo);
